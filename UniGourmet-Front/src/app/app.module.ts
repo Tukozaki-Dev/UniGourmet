7457d105815ce77949962abdefaf1962b8683071
Izabela Soares <soares.izabela96@gmail.com>
2022-10-10 14:29:35 -0300

    feat: creating modules

--- UniGourmet-Front/src/app/app.module.ts
+++ UniGourmet-Front/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,6 +13,7 @@ import { AngularMaterialModule } from './angular-material/angular-material.modul
     AppRoutingModule,
     BrowserAnimationsModule,
     AngularMaterialModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
