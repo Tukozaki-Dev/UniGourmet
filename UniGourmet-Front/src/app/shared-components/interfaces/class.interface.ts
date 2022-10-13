04df8658413171c7af58a4d796bab5d92b5d6288
Izabela Soares <soares.izabela96@gmail.com>
2022-10-12 22:25:30 -0300

    Creating interfaces and repositories files for admin module

---
+++ UniGourmet-Front/src/app/shared-components/interfaces/class.interface.ts
@@ -0,0 +1,7 @@
+
+export interface IClass {
+    name_code: string;
+    semester: number;
+    shift: string;
+    category: string;
+}
---
+++ UniGourmet-Front/src/app/shared-components/interfaces/subject.interface.ts
@@ -0,0 +1,7 @@
+
+export interface ISubject {
+    name: string;
+    registerCode: string;
+    semester: number;
+    numberOfClasses: number;
+}
