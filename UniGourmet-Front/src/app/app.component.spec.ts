bac74cb7de891f987b2b9f2bf5abf4b822eda550
GrazielyTukozaki <grazielybasilio@hotmail.com>
2023-01-06 16:38:32 -0300

    Adding professor, student and class unit tests to methods

--- UniGourmet-Front/src/app/app.component.spec.ts
+++ UniGourmet-Front/src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'UniGourmet-Front'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('UniGourmet-Front');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('UniGourmet-Front app is running!');
-  });
 });
